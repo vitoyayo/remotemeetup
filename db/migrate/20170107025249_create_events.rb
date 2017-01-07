a03378eb7897a707ace53b944d730d474374568f
Franzé Jr <franzejr@gmail.com>
2017-01-07 00:04:23 -0300

    First commit

---
+++ db/migrate/20170107025249_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.datetime :occurs_on
+      t.belongs_to :source, index: true
+
+      t.timestamps
+    end
+  end
+end
