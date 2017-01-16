6b4a1c22c6fbb5e462014da782417e9e491aced3
Franzé Jr <franzejr@gmail.com>
2017-01-16 17:45:24 -0300

    Changes after feedback

--- public/js/jquery.countTo.js
+++ public/js/jquery.countTo.js
@@ -19,8 +19,8 @@
   CountTo.DEFAULTS = {
     from: 0,               // the number the element should start at
     to: 0,                 // the number the element should end at
-    speed: 1000,           // how long it should take to count between the target numbers
-    refreshInterval: 100,  // how often the element should be updated
+    speed: 100,           // how long it should take to count between the target numbers
+    refreshInterval: 10,  // how often the element should be updated
     decimals: 0,           // the number of decimal places to show
     formatter: formatter,  // handler for formatting the value before rendering
     onUpdate: null,        // callback method for every time the element is updated
