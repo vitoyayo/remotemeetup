17096f864879f8054ec4ac2876e27c832a6c6e50
mose <mose@mose.com>
2017-02-07 00:07:22 +0800

    tuning puma conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,15 +62,15 @@ set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
 # set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
 # set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
 # set :puma_conf, "#{shared_path}/puma.rb"
-# set :puma_access_log, "#{shared_path}/log/puma_access.log"
-# set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
 # set :puma_role, :app
 # set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-# set :puma_threads, [0, 16]
-# set :puma_workers, 0
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 # set :puma_worker_timeout, nil
-# set :puma_init_active_record, false
-# set :puma_preload_app, false
+set :puma_init_active_record, true
+set :puma_preload_app, true
 # set :puma_plugins, []  #accept array of plugins
 # set :nginx_use_ssl, false
 
